6f08dfc892eed2ac252423f534b49b15c68bf77e
Andareon <Shcerbacov.Andrew94@gmail.com>
2018-11-04 14:16:21 +0300

    Удалено подключение пространства имен

--- ray.h
+++ ray.h
@@ -3,36 +3,34 @@
 
 #include "glm/geometric.hpp"
 
-using namespace glm;
-
 class Ray {
 private:
-    vec4 begin;
-    vec4 dir;
+    glm::vec4 begin;
+    glm::vec4 dir;
     int depth = 0;
-    ivec2 coords = {0, 0};
-    vec3 col = vec3(1);
+    glm::ivec2 coords = {0, 0};
+    glm::vec3 col = glm::vec3(1);
 
 public:
-    vec4 getBegin() const {return begin;}
+    glm::vec4 getBegin() const {return begin;}
 
-    void setBegin(vec4 bg) {begin=bg;}
+    void setBegin(glm::vec4 bg) {begin=bg;}
 
-    vec4 getDir() const {return normalize(dir);}
+    glm::vec4 getDir() const {return normalize(dir);}
 
-    void setDir(vec4 dr) {dir=dr;}
+    void setDir(glm::vec4 dr) {dir=dr;}
 
     int getDepth() const {return depth;}
 
     void setDepth(int dp) {depth=dp;}
 
-    ivec2 getCoords() const {return coords;}
+    glm::ivec2 getCoords() const {return coords;}
 
-    vec3 getCol() const {return col;}
+    glm::vec3 getCol() const {return col;}
 
-    void setCol(vec3 cl) {col=cl;}
+    void setCol(glm::vec3 cl) {col=cl;}
 
-    void reflect(vec4 bg, vec4 dr, vec3 cl) {
+    void reflect(glm::vec4 bg, glm::vec4 dr, glm::vec3 cl) {
         begin = bg;
         dir = normalize(dr);
         col *= cl;
@@ -43,7 +41,7 @@ public:
 
     void make_invalid() {depth = Config::get().MAX_RAY_REFLECTIONS;}
 
-    Ray(vec4 i, vec4 j, int k, ivec2 l) :begin(i), dir(normalize(j)), depth(k), coords(l) {}
+    Ray(glm::vec4 i, glm::vec4 j, int k, glm::ivec2 l) :begin(i), dir(normalize(j)), depth(k), coords(l) {}
 };
 
 #endif
